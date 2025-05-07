f097c430584ed61e3214c83017daaf9193fc01f4
IshwariKhopade <ishwarikhopade68@gmail.com>
2025-05-08 01:10:45 +0530

    Add files via upload

---
+++ Assignment2/Assignment2_1.c
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//  
+//  File Name : Assignment2_1.c
+//  Description : Used to one Number from User & Print that Number of * on Screen
+//  Author : Ishwari Bharat Khopade
+//  Date : 08/05/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    while(iCnt < iNo)
+    {
+        printf("*");
+        iCnt++;
+    }
+
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
