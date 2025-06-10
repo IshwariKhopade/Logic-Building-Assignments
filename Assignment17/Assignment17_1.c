753578e22d251220548e513c69429812dcb5e3ff
Ishwari Khopade <ishwarikhopade68@gmail.com>
2025-06-10 23:44:29 +0530

    Add files via upload

---
+++ Assignment17/Assignment17_1.c
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name : Assignment17_1.c
+//  Description : Write a program which accepts Number of rows and columns from user and Display Pattern.
+//  Input : 4 & 4
+//  Output : *   *   *   #
+//           *   *   #   *
+//           *   #   *   *
+//           #   *   *   *
+//  Author : Ishwari Bharat Khopade.
+//  Date : 10/06/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    for (i = iRow; i >= 1; i--)
+    {
+        for (j = iCol; j >= 1; j--)
+        {
+            if (i + j == iCol + 1)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+            
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter Number of Rows and Columns:\n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
---
+++ Assignment17/Assignment17_2.c
@@ -0,0 +1,52 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name : Assignment17_2.c
+//  Description : Write a program which accepts Number of rows and columns from user and Display Pattern.
+//  Input : 4 & 4
+//  Output : *   *   *   #
+//           *   *   #   @
+//           *   #   @   @
+//           #   @   @   @
+//  Author : Ishwari Bharat Khopade.
+//  Date : 10/06/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    for (i = iRow; i >= 1; i--)
+    {
+        for (j = iCol; j >= 1; j--)
+        {
+            if (i + j == iCol + 1)
+            {
+                printf("#\t");
+            }
+            else if (i + j <= iCol)
+            {
+                printf("@\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter Number of Rows and Columns:\n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
