69bdc842d2716bbf0807c62b363ec4b75f346911
Ishwari Khopade <ishwarikhopade68@gmail.com>
2025-06-10 23:41:33 +0530

    Add files via upload

---
+++ Assignment15/Assignment15_3.c
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name : Assignment15_3.c
+//  Description : Write a program which accepts Number of rows and columns from user and Display Pattern.
+//  Input : 5 & 5
+//  Output : $   *   *   *   *
+//           *   $   *   *   *
+//           *   *   $   *   *
+//           *   *   *   $   *
+//           *   *   *   *   $
+//  Author : Ishwari Bharat Khopade.
+//  Date : 10/06/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+            if (i == j)
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter Number of Rows and Columns:\n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
